42d47789bdcf7895e9886fdf42b6863e8fa099ba
Richard Palethorpe <rpalethorpe@suse.com>
2021-01-08 11:25:53 +0000

    Use sassc

--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -9,8 +9,8 @@ safely () {
 
 safely git branch --force gh-pages HEAD
 safely git checkout gh-pages
-safely sass --update css/reveal-override.scss
+safely sassc -m -t compact css/reveal-override.scss css/reveal-override.css
 safely git add --force css/*.css css/*.map
 safely git commit --message 'Latest .css and .css.map files for publishing via gh-pages'
-safely git push --force github gh-pages
+safely git push --force gh gh-pages
 safely git checkout -
